a564f2b340da7a3f6646087a980889c92ebb755c
Teddyawoke <awoketeddy@gmail.com>
2023-05-23 21:42:11 +0300

    installed packages

--- blogfrontend/sanity.config.ts
+++ blogfrontend/sanity.config.ts
@@ -3,15 +3,16 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
-  name: 'default',
-  title: 'exploreblogbackend',
-
-  projectId: 'c8ypz67g',
-  dataset: 'production',
-
+  basePath: '/studio',
+  name: 'Explore_Blog_Content_Studio',
+  title: 'Explore Blog Content Studio',
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
